65132baeb05d588c7e532ce8faf347d803b6b3e7
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-02-02 03:05:58 -0500

    Update git_config_script.sh

--- git_config_script.sh
+++ git_config_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-
 ssh-keygen -t rsa -b 4096 -C "local_vm_generic_default_user_email.com"
+
 # [Press Enter]
 # [Press Enter]
 # [Press Enter]
+
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
@@ -12,15 +13,15 @@ ssh-add ~/.ssh/id_rsa
 
 sudo apt install git-all
 git clone git@github.com:wiehehq/vm_bash_scripting.git
+
 # [yes]
+
 cd vm_bash_scripting
-git status
 cp ~/git_config_script.sh git_config_script.sh
 rm ~/git_config_script.sh
+echo "A new script should go here" >> generic_script_placeholder.sh
 git add .
 git config --global user.email "local_vm_generic_default_user_email.com"
 git config --global user.name "local_vm_generic_default_user_name"
 git commit -m "This is the test message for the test commit for the test file"
-git status
 git push origin master
-git status
