f444e8a2554569d53f2b7de260636e9e28279040
local_vm_generic_default_user_name <local_vm_generic_default_user_email.com>
2020-02-02 03:40:44 -0500

    file for installing curl, Atom, and Chrome

---
+++ add_tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install CURL
+sudo apt install curl
+
+# Install Atom
+sudo snap install atom --classic
+
+# Install Chrome
+sudo apt install gdebi-core
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd_64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb
