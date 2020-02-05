44916864435f56ff3f28293af211f1d4efdef672
local_vm_generic_default_user_name <local_vm_generic_default_user_email.com>
2020-02-04 20:43:08 -0500

    Script for building super basic PERN stack. Will be looking into making this fully automated, and then will move script elsewhere

---
+++ build_pern_stack.sh
@@ -0,0 +1,48 @@
+#!bin/bash
+
+# Update/Upgrade
+sudo apt update
+sudo apt upgrade
+
+# Set up Postgres
+sudo apt install postgresql postgresql-contrib
+sudo service postgresql start
+
+# Set up Node.js
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo npm init
+
+# Set up Express
+sudo npm install express
+
+# Set up Nodemon
+sudo npm install -g nodemon
+sudo npm install --save-dev nodemon
+touch app.js
+
+# Edit app.js
+# atom app.js
+              # const http = require('http');
+              #
+              # const hostname = '127.0.0.1';
+              # const port = 3000;
+              #
+              # const server = http.createServer((req, res) => {
+              #  res.statusCode = 200;
+              #  res.setHeader('Content-Type', 'text/plain');
+              #  res.end('Hello World');
+              # });
+              #
+              # server.listen(port, hostname, () => {
+              #  console.log(`Server running at http://${hostname}:${port}/`);
+              # });
+
+# Start application
+nodemon app.js
+
+# Set up React
+sudo npm install -g create-react-app
+npx create-react-app awesome-project
+cd awesome-project
+npm start
