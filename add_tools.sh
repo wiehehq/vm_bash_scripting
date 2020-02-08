fdf094f4d06b9267dc77ee14a9c4e59fc206022a
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-02-07 21:14:18 -0500

    Update add_tools.sh

--- add_tools.sh
+++ add_tools.sh
@@ -8,5 +8,5 @@ sudo snap install atom --classic
 
 # Install Chrome
 sudo apt install gdebi-core
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd_64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo gdebi google-chrome-stable_current_amd64.deb
