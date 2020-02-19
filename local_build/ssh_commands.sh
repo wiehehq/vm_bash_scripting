5ac27253c66c08828b42a06655f384cc20d650cd
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-02-19 01:08:25 -0500

    Format with line breaks

--- local_build/ssh_commands.sh
+++ local_build/ssh_commands.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo '----   mkdir Development   ----'
+function show_command() {
+  echo '';
+  echo $1
+  echo '';
+}
+
+show_command '----   mkdir Development   ----'
 eval "$(mkdir Development)"
 echo '';
-echo '----   ssh-keygen -t rsa -b 4096 -C "local_vm_generic_default_user_email.com" -f "/home/dev/.ssh/id_rsa"   ----'
+show_command '----   ssh-keygen -t rsa -b 4096 -C "local_vm_generic_default_user_email.com" -f "/home/dev/.ssh/id_rsa"   ----'
 eval "$(ssh-keygen -t rsa -b 4096 -C 'my_email' -f '/home/dev/.ssh/id_rsa')"
 echo '';
-echo '----   eval "$(ssh-agent -s)"   -----'
+show_command '----   eval "$(ssh-agent -s)"   -----'
 eval "$(ssh-agent -s)"
 echo '';
-echo '----   ssh-add ~/.ssh/id_rsa   -----'
+show_command '----   ssh-add ~/.ssh/id_rsa   -----'
 eval "$(ssh-add ~/.ssh/id_rsa)"
 echo '';
 
