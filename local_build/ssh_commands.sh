ea734fb51a55b4ffcafba6000555ca5899596581
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-02-19 01:13:40 -0500

    Reformat shell notes

--- local_build/ssh_commands.sh
+++ local_build/ssh_commands.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 function show_command() {
-  echo '';
+  echo '--------------------------------------';
   echo $1
-  echo '';
+  echo '--------------------------------------';
 }
 
-show_command '----   mkdir Development   ----'
+show_command 'mkdir Development'
 eval "$(mkdir Development)"
 echo '';
-show_command '----   ssh-keygen -t rsa -b 4096 -C "local_vm_generic_default_user_email.com" -f "/home/dev/.ssh/id_rsa"   ----'
+show_command 'ssh-keygen -t rsa -b 4096 -C "local_vm_generic_default_user_email.com" -f "/home/dev/.ssh/id_rsa"'
 eval "$(ssh-keygen -t rsa -b 4096 -C 'my_email' -f '/home/dev/.ssh/id_rsa')"
 echo '';
-show_command '----   eval "$(ssh-agent -s)"   -----'
+show_command 'eval "$(ssh-agent -s)"'
 eval "$(ssh-agent -s)"
 echo '';
-show_command '----   ssh-add ~/.ssh/id_rsa   -----'
+show_command 'ssh-add ~/.ssh/id_rsa'
 eval "$(ssh-add ~/.ssh/id_rsa)"
 echo '';
 
