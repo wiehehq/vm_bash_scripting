e06f5ce993bf38475ae19a8f38c01e4262d1211d
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-02-19 01:02:17 -0500

    basic ssh commands for testing

--- local_build/ssh_commands.sh
+++ local_build/ssh_commands.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo '----   mkdir Development   ----'
+eval "$(mkdir Development)"
+echo '';
 echo '----   ssh-keygen -t rsa -b 4096 -C "local_vm_generic_default_user_email.com" -f "/home/dev/.ssh/id_rsa"   ----'
-echo 'ssh-keygen -t rsa -b 4096 -C "local_vm_generic_default_user_email.com" -f "/home/dev/.ssh/id_rsa"'
+eval "$(ssh-keygen -t rsa -b 4096 -C 'my_email' -f '/home/dev/.ssh/id_rsa')"
+echo '';
 echo '----   eval "$(ssh-agent -s)"   -----'
-echo 'eval "$(ssh-agent -s)"'
+eval "$(ssh-agent -s)"
+echo '';
 echo '----   ssh-add ~/.ssh/id_rsa   -----'
-echo ssh-add ~/.ssh/id_rsa
-echo '----   ssh-add ~/.ssh/id_rsa   -----'
+eval "$(ssh-add ~/.ssh/id_rsa)"
+echo '';
 
 exit
