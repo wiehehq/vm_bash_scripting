d05d5ef01c38055555c75427d82b59b810f29b68
wiehehq <60562268+wiehehq@users.noreply.github.com>
2020-02-19 20:37:53 -0500

    Fix some quote mark stuff

--- local_build/ssh_commands.sh
+++ local_build/ssh_commands.sh
@@ -12,14 +12,12 @@ echo
 read -p 'Do you want to use defaults? ' defaults_requested
 if [ "$defaults_requested" = "Y" ] ; then
   email='generic_email.com'
-  ssh_path='~/.ssh'
-  filename='id_rsa'
-  passphrase=''
+  ssh_path='~/.ssh/id_rsa'
+  passphrase='test_passphrase'
 elif [ "$defaults_requested" = "N" ] ; then
   echo
   read -p 'Enter email: ' email
-  read -p 'Enter SSH path (do not put slash at end): ' ssh_path
-  read -p 'Enter key filename: ' filename
+  read -p 'Enter SSH path with filename: ' ssh_path
   read -s -p 'Enter passphrase you want to use: ' passphrase
   echo
 fi
@@ -27,16 +25,15 @@ fi
 echo
 echo "email ------ [$email]"
 echo "ssh_path --- [$ssh_path]"
-echo "filename --- [$filename]"
 echo "passphrase - [$passphrase]"
 
 echo
 
-# show_command 'eval "$(ssh-keygen -t rsa -b 4096 -C "$email" -f "$ssh_path/$filename")"'
-eval "$(ssh-keygen -t rsa -b 4096 -C '$email' -f "$ssh_path/$filename" -P "$passphrase")"
-# show_command 'eval "$(ssh-agent -s)"'
+show_command 'eval "$(ssh-keygen -t rsa -b 4096 -C $email -f $ssh_path -P $passphrase)"'
+eval "$(ssh-keygen -t rsa -b 4096 -C $email -f $ssh_path -P $passphrase)"
+show_command 'eval "$(ssh-agent -s)"'
 eval "$(ssh-agent -s)"
-# show_command 'ssh-add ~/.ssh/id_rsa'
-eval "$(ssh-add ~/.ssh/id_rsa)"
+show_command 'ssh-add $ssh_path'
+eval "$(ssh-add $ssh_path)"
 
 exit
